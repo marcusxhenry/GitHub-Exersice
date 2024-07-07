6cfe5cfc1e9e0767fbeb926bd861024dc68c7657
Marcus Henry <marcusxhenrys@gmail.com>
2024-07-07 09:37:16 -0700

    Initial Commit

---
+++ GitHub Exersice/GitHub_ExersiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExersiceApp.swift
+//  GitHub Exersice
+//
+//  Created by Owner on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExersiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
